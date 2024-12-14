580de2cb7c1db189cd06b23ac4d3fcb7b072cc91
Nicklas <gta5.r6s.bf4@gmail.com>
2024-12-14 22:21:51 +0100

    Initial commit

---
+++ QuantumKat/Program.cs
@@ -0,0 +1,35 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace QuantumKat;
+
+class Program
+{
+    private static DiscordSocketClient _client;
+
+    public static async Task Main(string[] args)
+    {
+        DiscordSocketConfig config = new()
+        {
+            GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+        };
+
+        _client = new DiscordSocketClient(config);
+
+        _client.Log += LogAsync;
+        _client.Ready += ReadyAsync;
+    }
+
+    private static Task ReadyAsync()
+    {
+        System.Console.WriteLine();
+        return Task.CompletedTask;
+    }
+
+    private static Task LogAsync(LogMessage message)
+    {
+        System.Console.WriteLine(message.ToString());
+        return Task.CompletedTask;
+    }
+}
