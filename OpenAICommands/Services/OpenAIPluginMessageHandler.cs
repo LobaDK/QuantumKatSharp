8e53e42cffeb27b89e600d2307906687ea56e377
Nicklas <gta5.r6s.bf4@gmail.com>
2025-04-27 02:53:10 +0200

    Add OpenAICommands project and implement OpenAIPluginMessageHandler for message handling

---
+++ OpenAICommands/Services/OpenAIPluginMessageHandler.cs
@@ -0,0 +1,28 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using QuantumKat.Interfaces;
+using System.Text.RegularExpressions;
+
+namespace OpenAICommands.Services;
+
+public class OpenAIPluginMessageHandler : IMessageHandlerPlugin
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly DiscordSocketClient _client;
+    
+    public OpenAIPluginMessageHandler(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+        _client = _serviceProvider.GetRequiredService<DiscordSocketClient>();
+    }
+
+    public async Task HandleMessageAsync(SocketUserMessage message, int argPos)
+    {
+        string pattern = $@"^hey[, ]+(quantumkat|<@{_client.CurrentUser.Id}>)";
+        if (Regex.IsMatch(message.Content, pattern, RegexOptions.IgnoreCase))
+        {
+            await message.ReplyAsync("Hello!");
+        }
+    }
+}
